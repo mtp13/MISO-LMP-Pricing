c16183704fba15d69a7a6fe5cb6efd78b84badac
Mike Pullen <mike.pullen@gmail.com>
2012-11-01 21:04:17 -0500

    rearranged code, clean-up

--- LMPViewController.m
+++ LMPViewController.m
@@ -18,6 +18,40 @@
 @synthesize onPeakDisplay = _onPeakDisplay;
 @synthesize offPeakDisplay = _offPeakDisplay;
 
+- (IBAction)changeLMPDate:(id)sender
+{
+    NSDate *LMPDate;
+
+    int i = [sender selectedSegmentIndex];
+    if (i == 0) {
+        LMPDate = [self getYesterdaysDate];
+        [self resetDisplay:LMPDate];
+        [self fetchLMPDailyFile:LMPDate];
+    }
+    if (i == 1) {
+        LMPDate = [NSDate date];
+        [self resetDisplay:LMPDate];
+        [self fetchLMPDailyFile:LMPDate];
+    }
+    if (i == 2) {
+        LMPDate = [self getTomorrowsDate];
+        [self resetDisplay:LMPDate];
+        [self fetchLMPDailyFile:LMPDate];
+    }
+
+}
+
+- (void)fetchLMPDailyFile:(NSDate *)aDate
+{
+    NSDate *LMPDate = aDate;
+    NSDictionary *hourlyPrices = [LMPDayAheadFetcher fetchForDate:LMPDate];
+    NSArray *a = [hourlyPrices objectForKey:@"EEI_Interface_LMP"];
+    self.onPeakDisplay.text = [NSString stringWithFormat:@"On Peak = $%.2f",
+                               [LMPDayAheadFetcher getONPeakAverage:a]];
+    self.offPeakDisplay.text = [NSString stringWithFormat:@"Off Peak = $%.2f",
+                               [LMPDayAheadFetcher getOFFPeakAverage:a]];
+}
+
 - (NSDate *)getYesterdaysDate
 {
     NSDateComponents *componentsToSubtract = [[NSDateComponents alloc] init];
@@ -36,13 +70,13 @@
     [componentsToAdd setDay:1];
 
     NSDate *tomorrow = [[NSCalendar currentCalendar]
-                         dateByAddingComponents:componentsToAdd
-                         toDate:[NSDate date]
-                         options:0];
+                        dateByAddingComponents:componentsToAdd
+                        toDate:[NSDate date]
+                        options:0];
     return tomorrow;
 }
 
--(void)updateDisplay:(NSDate *)aDate
+-(void)resetDisplay:(NSDate *)aDate
 {
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
@@ -52,41 +86,6 @@
 
     self.onPeakDisplay.text = @"On Peak = $00.00";
     self.offPeakDisplay.text = @"Off Peak = $00.00";
-}
-
-- (IBAction)changeLMPDate:(id)sender
-{
-    NSDate *LMPDate;
-
-
-    int i = [sender selectedSegmentIndex];
-    if (i == 0) {
-        LMPDate = [self getYesterdaysDate];
-        [self updateDisplay:LMPDate];
-        [self fetchLMPDailyFile:LMPDate];
-    }
-    if (i == 1) {
-        LMPDate = [NSDate date];
-        [self updateDisplay:LMPDate];
-        [self fetchLMPDailyFile:LMPDate];
-    }
-    if (i == 2) {
-        LMPDate = [self getTomorrowsDate];
-        [self updateDisplay:LMPDate];
-        [self fetchLMPDailyFile:LMPDate];
-    }
-
-}
-
-- (void)fetchLMPDailyFile:(NSDate *)aDate
-{
-    NSDate *LMPDate = aDate;
-    NSDictionary *hourlyPrices = [LMPDayAheadFetcher fetchForDate:LMPDate];
-    NSArray *a = [hourlyPrices objectForKey:@"EEI_Interface_LMP"];
-    self.onPeakDisplay.text = [NSString stringWithFormat:@"On Peak = $%.2f",
-                               [LMPDayAheadFetcher getONPeakAverage:a]];
-    self.offPeakDisplay.text = [NSString stringWithFormat:@"Off Peak = $%.2f",
-                               [LMPDayAheadFetcher getOFFPeakAverage:a]];
 }
 
 @end
