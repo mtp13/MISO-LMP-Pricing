ecba89b2e17497ee938996d593610c576717abaa
Mike Pullen <mike.pullen@gmail.com>
2013-03-07 18:13:54 -0600

    added PricesTVC controllers (not wired up yet)

---
+++ PricesTVC.h
@@ -0,0 +1,14 @@
+//
+//  PricesTVC.h
+//  EEI
+//
+//  Created by Mike Pullen on 3/6/13.
+//  Copyright (c) 2013 Mike Pullen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Prices.h"
+
+@interface PricesTVC : UITableViewController
+@property (nonatomic, strong) Prices *prices;
+@end
